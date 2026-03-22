1c7f54ace4c57d753587cbff8b882dcb9bc2c725
Yannick Pulver <yannick@appswithlove.com>
2026-03-22 16:32:13 +0100

    Add macOS code signing, notarization, and multi-platform CI

--- forge.config.ts
+++ forge.config.ts
@@ -11,6 +11,12 @@ import { PublisherGithub } from '@electron-forge/publisher-github';
 const config: ForgeConfig = {
   packagerConfig: {
     asar: true,
+    osxSign: {},
+    osxNotarize: {
+      appleId: process.env.APPLE_ID!,
+      appleIdPassword: process.env.APPLE_APP_PASSWORD!,
+      teamId: process.env.APPLE_TEAM_ID!,
+    },
   },
   rebuildConfig: {},
   makers: [
